df116b2ebb819d595a201441c6647e01d03a8d6a
maaafiqs <163867131+maaafiqs@users.noreply.github.com>
2024-07-07 11:35:06 +0800

    Bebas.java

--- Bebas.java
+++ Bebas.java
@@ -2,12 +2,11 @@ public class Bebas {
     private String menuutama;
     private String perhitungan_nilai;
     private String daftar_nilai;
+    private String ranking;
 
 public void menuUtama(){
-
     System.out.println("Ini method untuk menu utama");
 }
-
 
 public String getMenuutama(){
 
@@ -21,28 +20,46 @@ public void setMenuutama(String menuutama){
 
 
 public void daftarNilai(){
-
     System.out.println("Ini method Daftar Nilai");
 }
 
-
 public String getDaftarNilai(){
-
     return this.daftar_nilai;
-
 }
 
 public void setDaftarNilai(String daftar_nilai){
 
     this.daftar_nilai = daftar_nilai;
-
 }
 
 
+public void perhitunganNilai(){
 
-public String perhitunganNilai(){
+    System.out.println("Ini method Perhitungan Nilai");
+}
+
+public String getPerhitunganNilai(){
 
     return this.perhitungan_nilai;
+}
+
+public void setPerhitunganNilai(String perhitungan_nilai){
+
+    this.perhitungan_nilai = perhitungan_nilai;
+}
+
+
+public void ranking(){
+
+    System.out.println("Ini method Ranking");
+}
+
+public String getRanking(){
+    return this.ranking;
+}
+
+public void setRanking(String ranking){
+    this.ranking = ranking;
 }
 
 }
